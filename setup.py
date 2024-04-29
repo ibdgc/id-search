5d22dcc725710e7697ff41d1fc42b28ecbc6b64a
Christopher Tastad <ctastad@gmail.com>
2024-04-28 20:05:39 -0500

    reworked setup process

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
 from setuptools import setup, find_namespace_packages
 
-with open('README.rst', 'r') as fh:
-    long_description = fh.read()
+
+def read_requirements():
+    with open('requirements.txt', 'r') as file:
+        return [line.strip() for line in file.readlines()]
+
 
 setup(
-    name='ibdgc-db',
-    version='0.0.1',
-    author='Phil Schumm',
-    author_email='pschumm@uchicago.edu',
-    description='Tools for managing information on IBDGC research participants',
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
-    url='https://rcg.bsd.uchicago.edu/gitlab/ibdgc/dcc/ibdgc-db',
+    name='id-search',
+    version='0.1.0',
+    description='IBDGC ID Database Search Tool',
+    long_description_content_type='text/markdown',
+    url='https://github.com/ibdgc/id-search',
+    author='Christopher Tastad',
+    author_email='christopher.tastad@mssm.edu',
+    license='MIT',
     packages=find_namespace_packages(where='.'),
-    install_requires=[
-        'sqlalchemy~=2.0.25',
-        'click-config-file~=0.6.0',
-        'dataforge[redcap] @ git+https://gitlab.com/pschumm/data-forge.git@c46c2f483f6cca4007c85d4d10b9ef3edb29cef8',
-        'eralchemy2~=1.3.8'
-    ],
-    include_package_data=True,
-    entry_points='''
-        [console_scripts]
-        ibdgc-db=ibdgc_db.cli:cli
-    ''',
+    install_requires=read_requirements(),
+    include_package_data=True,  # Ensure package data is included
+    entry_points={
+        'console_scripts': [
+            'id-search=id_search.cli:cli'
+        ]
+    },
 )
